71dbb64e8dc01daf0d611e53365d7fbee1452fd8
aibekov <aibek.zhumabekov@yahoo.com>
2020-05-09 14:58:01 -0500

    adding redux and building components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import Sidebar from './components/Sidebar/Sidebar'
 import Main from  './components/Main/Main'
+import store from './store'
+import _ from 'lodash'
 import './App.css';
 
 
 const App = ()=> {
+  const {contacts} = store.getState()
   return (
     <div className="App">
-      <Sidebar/>
+      <Sidebar contacts={_.values(contacts)}/>
       <Main/>
     </div>
   );
